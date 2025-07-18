bac4d34bd601903cb0ad384b19d48f2f16201f41
BIANCA_TAKAMORI <biancahelenatakamoriribeiro@gmail.com>
2025-07-18 14:15:52 -0300

    deploy fly.io

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'New Word Everyday Game API is running! 🚀';
   }
 }
