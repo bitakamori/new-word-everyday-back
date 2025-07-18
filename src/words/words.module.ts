759a2a598c8bcf10dc6607b820190114b8b278c9
BIANCA_TAKAMORI <biancahelenatakamoriribeiro@gmail.com>
2025-07-18 16:16:58 -0300

    add daily limit words

--- src/words/words.module.ts
+++ src/words/words.module.ts
@@ -2,13 +2,14 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { HttpModule } from '@nestjs/axios';
 import { Word } from './word.entity';
+import { User } from '../users/user.entity';
 import { WordsService } from './words.service';
 import { WordsController } from './words.controller';
 import { UsersModule } from '../users/users.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Word]),
+    TypeOrmModule.forFeature([Word, User]),
     HttpModule,
     UsersModule,
   ],
